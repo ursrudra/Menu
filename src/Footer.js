b7928619f77c91c8dd27b51665b6c58706674816
Rudra Kishore <ursrudra@users.noreply.github.com>
2018-04-12 03:23:08 +0530

    Updated Footer.js
    fixed footer bug

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Footer = () =>{
 	    	return (
-			<div className="p-2 fixed-bottom">&copy; Rudra Kishore - 2018</div>
+			<div className="p-2 position-absolute">&copy; Rudra Kishore - 2018</div>
 			);
 
 }
